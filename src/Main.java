0dce49ce506c2781e7718a5e6f2bf430f287f0a3
JOHNKYON <zhaofengbo1321@163.com>
2018-03-07 22:22:56 +0900

    Finish TwoSum questions

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import questions.twoSum.TwoSum;
+
+import java.util.Arrays;
 
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args){
+        TwoSum twoSum = new TwoSum();
+        System.out.println(Arrays.toString(twoSum.twoSum(new int[]{1, 2, 3, 4, 5}, 8)));
     }
+
 }
---
+++ src/questions/twoSum/TwoSum.java
@@ -0,0 +1,27 @@
+/**
+ * Created by JOHNKYON on 2018/3/7.
+ * This is a solution to LeetCode Questions.
+ */
+
+package questions.twoSum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public TwoSum() {
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> myMap= new HashMap<>();
+        for (int i = 0; i< nums.length; i++){
+            int complement = target - nums[i];
+            if (myMap.containsKey(complement)){
+                return new int[] {myMap.get(complement), i};
+            }
+            myMap.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
