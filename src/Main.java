aff08c0c2e8279e983e90d7b44a1fd54c75987d1
JOHNKYON <zhaofengbo1321@163.com>
2018-03-13 19:33:11 +0900

    Finishing AC madian of two sorted arrays

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import questions.medianOfTwoSortedArrays.MedianOfTwoSortedArrays;
 public class Main {
     public static void main(String[] args){
         MedianOfTwoSortedArrays median = new MedianOfTwoSortedArrays();
-        int[] nums1 = {1,3};
-        int[] nums2 = {2};
+        int[] nums1 = {1,2,3};
+        int[] nums2 = {4,5,6};
         System.out.print(median.findMedian(nums1, nums2));
     }
 
--- src/questions/medianOfTwoSortedArrays/MedianOfTwoSortedArrays.java
+++ src/questions/medianOfTwoSortedArrays/MedianOfTwoSortedArrays.java
@@ -21,55 +21,147 @@ public class MedianOfTwoSortedArrays {
 //        Make sure that num1 points to the shorter one
         if (len1 > len2){
             int[] temp = nums2;
-            int templen = len2;
             nums2 = nums1;
             nums1 = temp;
 //            len2 = len1;
-            len1 = templen;
+            len1 = len2;
         }
-        int index1 = nums1.length-1;
+
         int index2;
         int midLen;
         if (flag) {midLen = total/2;}
         else {midLen = (total+1)/2;}
-        index2 = midLen - index1-2;
-        int left = 0, right = nums1.length-1;
+        int left = 0, right = nums1.length;
+        int index1 = (left + right) / 2;
+        index2 = midLen - index1;
 
-        if (0 == len1 || nums1[right] < nums2[0] ){
-//            Every elements in num1 is smaller than any element in num2
+
+        if (0 == len1 || nums1[right-1] < nums2[0] ){
+//            Every elements in num1 is smaller than any element in num2 or num1 is empty.
             if (flag){
 //                Even
-                if (index2 >= 0) {
-                    return ((double) nums2[index2] + (double) nums2[index2 + 1]) / 2;
+                if (midLen - len1 > 0) {
+                    return ((double) nums2[midLen - len1 -1] + (double) nums2[midLen - len1]) / 2;
                 }else {
-                    return ((double) nums1[index1] + (double) nums2[index2 + 1]) / 2;
+                    if (0 == len1) {
+                        return (double) 0;
+                    }else {
+                        return ((double) nums2[0] + (double) nums1[len1-1]) / 2;
+                    }
                 }
             }else {
-                return nums2[index2];
+                return nums2[midLen-len1-1];
             }
         }else {
-            int index1R = index1;   //For the first time run, the index1 is at the edge,so we have to set the bigger one
-                                    //to make sure it doesn't go beyond the border.
-            int index2R = index2 + 1;
-            if (index2 < 0) { index2 = 0;}
-            while (nums1[index1R] < nums2[index2] || nums2[index2R] < nums1[index1]){
-                if (index1 == (left+right)/2){break;}
-                index1 = (left+right)/2;
-                if (nums1[index1R] < nums2[index2]){    //index1 is too small
-                    left = index1;
-                } else {                                //index1 is too big
+            int index1L = index1 - 1;
+            if (0 == index1) {
+                index1L = index1;   //For the first time run, the index1 is at the edge,so we have to set the bigger one
+                //to make sure it doesn't go beyond the border.
+            }
+            int index2R = index2;
+            if (index2 == nums2.length){
+                index2R = index2-1;
+            }
+//            if (0 == index2) { index2L = 0;}
+            while (nums1[index1] < nums2[index2-1] || nums2[index2R] < nums1[index1L]){
+                if (nums1[index1] < nums2[index2-1]){       //Index1 is too small
+                    if (index1 == len1){                    //All elements in num1 are in the smaller set
+                        if (flag){
+                            double small = Math.max(nums1[index1-1], nums2[index2-1]);
+                            double big;
+                            if (index1 == nums1.length){                //All elements in num1 are in the smaller set
+                                big = nums2[index2];
+                            }else {
+                                big = Math.min(nums1[index1], nums2[index2R]);
+                            }
+                            return (small + big)/2;
+                        }
+                        else {return Math.max(nums1[index1L], nums2[index2-1]);}
+                    }else {
+                        left = index1;
+                        if (index1 == (left+right+1)/2){break;}
+                        index1 = (left + right + 1) / 2;
+                    }
+                } else {                                    //Index1 is too big
+                    if (0 == index1){
+                        if (flag){
+//                Even
+                            if (index2 == nums2.length || nums2[index2] > nums1[index1]) {
+                                return ((double) nums2[index2-1] + (double) nums1[index1]) / 2;
+                            }else {
+                                return ((double) nums2[index2-1] + (double) nums2[index2]) / 2;
+                            }
+                        }else {
+                            return nums2[index2-1];
+                        }
+                    }
                     right = index1;
+                    if (index1 == (left+right)/2){break;}
+                    index1 = (left + right) / 2;
                 }
-                index1R = index1 + 1;
-                index2 = midLen - index1 - 2;
-                index2R = index2 + 1;
+                index1L = index1-1;
+                index2 = midLen - index1;
+                index2R = index2;
+                if (0 == index1) {                           //All elements in num1 are in the bigger set
+                    if (flag){
+//                Even
+                        if (index2 == nums2.length || nums2[index2] > nums1[index1]) {
+                            return ((double) nums2[index2-1] + (double) nums1[index1]) / 2;
+                        }else {
+                            return ((double) nums2[index2-1] + (double) nums2[index2]) / 2;
+                        }
+                    }else {
+                        return nums2[index2-1];
+                    }
+                } else if (index1 == len1){                    //All elements in num1 are in the smaller set
+                    if (flag){
+                        double small = Math.max(nums1[index1-1], nums2[index2-1]);
+                        double big;
+                        if (index1 == nums1.length){                //All elements in num1 are in the smaller set
+                            big = nums2[index2];
+                        }else {
+                            big = Math.min(nums1[index1], nums2[index2R]);
+                        }
+                        return (small + big)/2;
+                    }
+                    else {return Math.max(nums1[index1L], nums2[index2-1]);}
+                }
+            }
+            if (0 == index1) {                           //All elements in num1 are in the bigger set
+                if (flag){
+//                Even
+                    if (index2 == nums2.length || nums2[index2] > nums1[index1]) {
+                        return ((double) nums2[index2-1] + (double) nums1[index1]) / 2;
+                    }else {
+                        return ((double) nums2[index2-1] + (double) nums2[index2]) / 2;
+                    }
+                }else {
+                    return nums2[index2-1];
+                }
+            } else if (index1 == len1){                    //All elements in num1 are in the smaller set
+                if (flag){
+                    double small = Math.max(nums1[index1-1], nums2[index2-1]);
+                    double big;
+                    if (index1 == nums1.length){                //All elements in num1 are in the smaller set
+                        big = nums2[index2];
+                    }else {
+                        big = Math.min(nums1[index1], nums2[index2R]);
+                    }
+                    return (small + big)/2;
+                }
+                else {return Math.max(nums1[index1L], nums2[index2-1]);}
             }
             if (flag){
-                double small = Math.max(nums1[index1], nums2[index2]);
-                double big = Math.min(nums1[index1R], nums2[index2R]);
+                double small = Math.max(nums1[index1L], nums2[index2-1]);
+                double big;
+                if (index1 == nums1.length){                //All elements in num1 are in the smaller set
+                    big = nums2[index2R];
+                }else {
+                    big = Math.min(nums1[index1], nums2[index2R]);
+                }
                 return (small + big)/2;
             }
-            else {return Math.max(nums1[index1], nums2[index2]);}
+            else {return Math.max(nums1[index1L], nums2[index2-1]);}
         }
     }
 }
