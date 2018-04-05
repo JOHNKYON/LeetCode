e605b81365b84460d2602e240e9548c2ba45f428
JOHNKYON <zhaofengbo1321@163.com>
2018-04-05 11:27:41 +0900

    leetcode 56 accepted

--- src/questions/leetcode56/MergeIntervals.java
+++ src/questions/leetcode56/MergeIntervals.java
@@ -1,38 +1,51 @@
 package questions.leetcode56;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by JOHNKYON on 2018/4/4.
  * This is a solution to LeetCode Questions.
  */
 public class MergeIntervals {
+
+    private class IntervalComparator implements Comparator<Interval> {
+        @Override
+        public int compare(Interval a, Interval b) {
+            return a.start < b.start ? -1 : a.start == b.start ? 0 : 1 ;
+        }
+    }
+
     public List<Interval> merge(List<Interval> intervals){
         if (intervals.size() == 0){
             return intervals;
         }
+        Collections.sort(intervals, new IntervalComparator());
 
-        List<Interval> res = new ArrayList<>();
+        LinkedList<Interval> res = new LinkedList<>();
         for (Interval interval : intervals){
-            if (res.size() == 0){
+//            if (res.size() == 0){
+//                res.add(interval);
+//            } else {
+//                int startIndex = 0, start = interval.start, endIndex = 0, end = interval.end;
+//                for (int i = 0; res.get(i).end < interval.end; i++){
+//                    if (res.get(i).start <interval.start){
+//                        startIndex = i;
+//                        start = interval.start;
+//                    }
+//                    endIndex = i;
+//                }
+//                end = (end < res.get(endIndex).end)? res.get(endIndex).end : end;
+//                for (int i = startIndex; i <= endIndex; i++){
+//                    res.remove(i);
+//                }
+//                res.add(new Interval(start, end));
+//            }
+//
+            if (res.isEmpty() || res.getLast().end < interval.start){
                 res.add(interval);
             } else {
-                int startIndex = 0, start = interval.start, endIndex = 0, end = interval.end;
-                for (int i = 0; res.get(i).end < interval.end; i++){
-                    if (res.get(i).start <interval.start){
-                        startIndex = i;
-                        start = interval.start;
-                    }
-                    endIndex = i;
-                }
-                end = (end < res.get(endIndex).end)? res.get(endIndex).end : end;
-                for (int i = startIndex; i <= endIndex; i++){
-                    res.remove(i);
-                }
-                res.add(new Interval(start, end));
+                res.getLast().end = Math.max(interval.end, res.getLast().end);
             }
-
         }
         return res;
     }
