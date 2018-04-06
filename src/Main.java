7e9183af0428a25623a0047cccd92c4a75a0d7a9
JOHNKYON <zhaofengbo1321@163.com>
2018-04-06 18:51:01 +0900

    leetcode140 accepted

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
-import questions.leetcode128.LongestConsecutiveSequence;
+import questions.leetcode140.WordBreak2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main {
     public static void main(String[] args){
-        LongestConsecutiveSequence method = new LongestConsecutiveSequence();
-        int[] nums = {100,4,200,1,3,2};
-//        List<InsertInterval.Interval> intervals = new ArrayList<InsertInterval.Interval>(){{
+        WordBreak2 method = new WordBreak2();
+
+        String s ="catsanddogs";
+        List<String> dic = new ArrayList<String>(){{
+            add("cats");
+            add("and");
+            add("dog");
+            add("cat");
+            add("sand");
+        }};
+        //        List<InsertInterval.Interval> intervals = new ArrayList<InsertInterval.Interval>(){{
 ////            add(method.createInterval(6,9));
 //            add(method.createInterval(1,5));
 //        }};
@@ -18,7 +29,7 @@ public class Main {
 //        for (InsertInterval.Interval i : method.insert(intervals, interval)){
 //            i.print();
 //        }
-        System.out.print(method.longestConsecutive(nums));
+        System.out.print(method.wordBreak(s, dic));
     }
 
 }
