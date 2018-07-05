117f490f86cd1b721ac6251580a1f01ca982330f
JOHNKYON <zhao_fengbo@outlook.com>
2018-07-05 11:30:47 +0900

    leetcode36 finished.

---
+++ src/questions/leetcode36/CombinationSum.java
@@ -0,0 +1,30 @@
+package questions.leetcode36;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        searchSum(candidates, res, new ArrayList<>(), target, 0);
+
+
+        return res;
+    }
+
+    private void searchSum(int[] nums, List<List<Integer>> res, List<Integer> temp_list, int target, int begin){
+        if (target < 0) return;
+        else if (target == 0) {
+            res.add(new ArrayList<>(temp_list));
+            return;
+        }
+        else {
+            for (int i = begin; i<nums.length; i++){
+                temp_list.add(nums[i]);
+                searchSum(nums, res, temp_list, target-nums[i], i);
+                temp_list.remove(temp_list.size()-1);
+            }
+        }
+    }
+}
