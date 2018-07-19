2249d4cb833723129cdad4e4527ec303897847ce
JOHNKYON <zhao_fengbo@outlook.com>
2018-07-19 22:29:06 +0900

    Leetcode114 Accepted.

---
+++ src/questions/leetcode114/FlattenBinaryTreetoLinkedList.java
@@ -0,0 +1,17 @@
+package questions.leetcode114;
+
+import questions.dataStructure.TreeNode;
+
+public class FlattenBinaryTreetoLinkedList {
+    private TreeNode prev = null;
+
+    public void flatten(TreeNode root) {
+        if (root == null)
+            return;
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
