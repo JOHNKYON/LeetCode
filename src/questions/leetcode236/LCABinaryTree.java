0fe41bc9577b3331b4092d6d1a682472a81901d8
JOHNKYON <zhao_fengbo@outlook.com>
2018-07-30 12:21:47 +0900

    Leetcode236 Accepted.

---
+++ src/questions/leetcode236/LCABinaryTree.java
@@ -0,0 +1,12 @@
+package questions.leetcode236;
+
+import questions.dataStructure.TreeNode;
+
+public class LCABinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        return left == null ? right : right == null ? left : root;
+    }
+}
