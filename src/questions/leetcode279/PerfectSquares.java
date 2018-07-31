c381c7be59d58113e3a5789d1fed9d9f121d51c1
JOHNKYON <zhaofengbo1321@163.com>
2018-07-31 13:48:16 +0900

    Leetcode279 1ms solution.

--- src/questions/leetcode279/PerfectSquares.java
+++ src/questions/leetcode279/PerfectSquares.java
@@ -13,4 +13,33 @@ public class PerfectSquares {
 
         return count[n];
     }
+
+    public int fastNumSquares(int n) {
+        /*
+        * Referring to Lagrange's Four Square Theorem.
+        * */
+        int sqr = (int)Math.sqrt(n);
+        if (isSquare(n))   return 1;
+
+        int temp = n;
+        while (temp%4 == 0){
+            temp /= 4;
+        }
+        if (temp%8 == 7)
+            return 4;
+
+        for(int i = 1; i<= sqr; i++){
+            temp = n;
+            temp -= i*i;
+            if (isSquare(temp)) return 2;
+        }
+
+        return 3;
+    }
+
+    private boolean isSquare(int n){
+        int sqr = (int)Math.sqrt(n);
+        if (sqr*sqr == n)   return true;
+        else    return false;
+    }
 }
