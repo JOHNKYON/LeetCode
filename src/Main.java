36b2fc68806b5106f156bb57dab5d301924037c6
JOHNKYON <zhaofengbo1321@163.com>
2018-08-02 16:16:01 +0900

    Leetcode300 Accepted.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-import questions.leetcode297.SerializeandDeserializeBinaryTree;
+import questions.leetcode300.LongestIncreasingSubsequence;
 
 public class Main {
     public static void main(String[] args) {
 
-        SerializeandDeserializeBinaryTree method = new SerializeandDeserializeBinaryTree();
+        LongestIncreasingSubsequence method = new LongestIncreasingSubsequence();
         //        List<InsertInterval.Interval> intervals = new ArrayList<InsertInterval.Interval>(){{
 ////            add(method.createInterval(6,9));
 //            add(method.createInterval(1,5));
@@ -17,8 +17,9 @@ public class Main {
 //        for (InsertInterval.Interval i : method.insert(intervals, interval)){
 //            i.print();
 //        }
-        String str = "1,2,3,null,null,4,5";
+        int[] nums = {4,10,4,3,8,9};
+//        int[] nums = {10,9,2,5,3,7,101,18};
 //        method.deserialize(str);
-        System.out.print(method.deserialize(str));
+        System.out.print(method.lengthOfLIS(nums));
     }
 }
