de219a49d1bac39e1baae252f16c7d84681ac1f5
JOHNKYON <zhao_fengbo@outlook.com>
2018-08-10 11:49:18 +0900

    Leetcode560 Accepted.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-import questions.Leetcode461.HammingDistance;
+import questions.leetcode560.SubarraySumEqualsK;
 
 public class Main {
     public static void main(String[] args) {
 
-        HammingDistance method = new HammingDistance();
+        SubarraySumEqualsK method = new SubarraySumEqualsK();
         //        List<InsertInterval.Interval> intervals = new ArrayList<InsertInterval.Interval>(){{
 ////            add(method.createInterval(6,9));
 //            add(method.createInterval(1,5));
@@ -17,11 +17,11 @@ public class Main {
 //        for (InsertInterval.Interval i : method.insert(intervals, interval)){
 //            i.print();
 //        }
-        int[][] queue = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+        int[] nums = {1,1,1};
 //        int[][] queue = {{1,0},{2,0},{3,0},{4,0},{5,0},{6,0},{7,0}};
 //        int[][] queue = {{2,4},{3,4},{9,0},{0,6},{7,1},{6,0},{7,3},{2,5},{1,1},{8,0}};
 
 //        method.deserialize(str);
-        System.out.print(method.hammingDistance(1,4));
+        System.out.print(method.subarraySum(nums,2));
     }
 }
---
+++ src/questions/leetcode560/SubarraySumEqualsK.java
@@ -0,0 +1,23 @@
+package questions.leetcode560;
+
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+    public int subarraySum(int[] nums, int k) {
+        if (nums.length == 0)   return 0;
+
+        int count = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0,1);
+        for (int n: nums){
+            sum += n;
+            if (map.containsKey(sum-k)){
+                count += map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+}
