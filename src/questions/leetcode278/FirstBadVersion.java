81924840d55c81b602a16de4ebd60f06964d7f8e
JOHNKYON <zhao_fengbo@outlook.com>
2018-08-19 10:56:19 +0900

    Leetcode278 Accepted.

---
+++ src/questions/leetcode278/FirstBadVersion.java
@@ -0,0 +1,18 @@
+package questions.leetcode278;
+
+public class FirstBadVersion {
+    public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int left = 0, right = n;
+            while (left < right){
+                // Watch out the overflow
+                int mid = left + (right - left)/2;
+                boolean curr = isBadVersion(mid);
+
+                if (curr)   right = mid;
+                else    left = mid+1;
+            }
+            return left;
+        }
+    }
+}
