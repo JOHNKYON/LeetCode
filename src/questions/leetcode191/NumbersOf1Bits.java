43e6b3b1221e63d48a1a4d2b34c4aff1faf945e6
JOHNKYON <zhao_fengbo@outlook.com>
2018-08-25 12:39:43 +0900

    Leetcode191 Accepted.

---
+++ src/questions/leetcode191/NumbersOf1Bits.java
@@ -0,0 +1,12 @@
+package questions.leetcode191;
+
+public class NumbersOf1Bits {
+    public int hammingWeight(int n) {
+        int sum = 0;
+        while (n != 0) {
+            sum++;
+            n &= (n - 1);
+        }
+        return sum;
+    }
+}
