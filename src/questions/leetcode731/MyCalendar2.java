59f2ba7d0c1e77f398f7b4dee4edd994261ac31f
JOHNKYON <zhao_fengbo@outlook.com>
2018-08-29 14:02:30 +0900

    Leetcode731, another solution.

--- src/questions/leetcode731/MyCalendar2.java
+++ src/questions/leetcode731/MyCalendar2.java
@@ -1,5 +1,7 @@
 package questions.leetcode731;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeMap;
 
 public class MyCalendar2 {
@@ -25,5 +27,25 @@ public class MyCalendar2 {
             }
         }
         return true;
+    }
+
+    private List<int[]> books = new ArrayList<>();
+    public boolean book2(int s, int e) {
+        MyCalendar overlaps = new MyCalendar();
+        for (int[] b : books)
+            if (Math.max(b[0], s) < Math.min(b[1], e)) // overlap exist
+                if (!overlaps.book(Math.max(b[0], s), Math.min(b[1], e))) return false; // overlaps overlapped
+        books.add(new int[]{ s, e });
+        return true;
+    }
+
+    private static class MyCalendar {
+        List<int[]> books = new ArrayList<>();
+        public boolean book(int start, int end) {
+            for (int[] b : books)
+                if (Math.max(b[0], start) < Math.min(b[1], end)) return false;
+            books.add(new int[]{ start, end });
+            return true;
+        }
     }
 }
