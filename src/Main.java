fec05287bc6981aad5213c8968d132e3711f6409
JOHNKYON <zhaofengbo1321@163.com>
2018-10-03 16:38:21 +0900

    Leetcode549 Accepted.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-import questions.leetcode560.SubarraySumEqualsK;
+import questions.dataStructure.TreeNode;
+import questions.leetcode549.BinaryTreeLongestConsecutiveSequenceII;
 
 public class Main {
     public static void main(String[] args) {
 
-        SubarraySumEqualsK method = new SubarraySumEqualsK();
+        BinaryTreeLongestConsecutiveSequenceII method = new BinaryTreeLongestConsecutiveSequenceII();
         //        List<InsertInterval.Interval> intervals = new ArrayList<InsertInterval.Interval>(){{
 ////            add(method.createInterval(6,9));
 //            add(method.createInterval(1,5));
@@ -22,6 +23,11 @@ public class Main {
 //        int[][] queue = {{2,4},{3,4},{9,0},{0,6},{7,1},{6,0},{7,3},{2,5},{1,1},{8,0}};
 
 //        method.deserialize(str);
-        System.out.print(method.subarraySum(nums,2));
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(4);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+        System.out.print(method.longestConsecutive(root));
     }
 }
