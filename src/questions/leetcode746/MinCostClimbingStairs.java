349fc56dc96cc368de9ec254d1f8c1fa68702cf9
JOHNKYON <zhao_fengbo@outlook.com>
2018-10-16 19:23:54 +0900

    Leetcode746 Accepted.

---
+++ src/questions/leetcode746/MinCostClimbingStairs.java
@@ -0,0 +1,14 @@
+package questions.leetcode746;
+
+public class MinCostClimbingStairs {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n];
+
+        for (int i=2; i<n; i++) {
+            dp[i] = Math.min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+        }
+
+        return Math.min(dp[n-1] + cost[n-1], dp[n-2] + cost[n-2]);
+    }
+}
