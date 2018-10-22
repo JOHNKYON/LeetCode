3aaf44240c5ce4b69f598d5d387342a51256131a
JOHNKYON <zhao_fengbo@outlook.com>
2018-10-22 13:36:28 +0900

    Leetcode174 Accepted.

---
+++ src/questions/leetcode174/DungeonGame.java
@@ -0,0 +1,26 @@
+package questions.leetcode174;
+
+public class DungeonGame {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int n = dungeon.length;
+        int m = dungeon[0].length;
+
+        int[][] dp = new int[n][m];
+
+        for (int i=n-1; i>=0 ;i--) {
+            for (int j=m-1; j>=0; j--) {
+                if (i==n-1 && j==m-1) {
+                    dp[i][j] = Math.max(1, 1-dungeon[i][j]);
+                } else if (i == n-1) {
+                    dp[i][j] = Math.max(1, dp[i][j+1] - dungeon[i][j]);
+                } else if (j == m-1) {
+                    dp[i][j] = Math.max(1, dp[i+1][j] - dungeon[i][j]);
+                } else {
+                    dp[i][j] = Math.min(Math.max(1, dp[i][j+1] - dungeon[i][j]),
+                            Math.max(1, dp[i+1][j] - dungeon[i][j]));
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
