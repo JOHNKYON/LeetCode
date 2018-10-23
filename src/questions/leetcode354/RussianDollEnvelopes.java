ba26d291ed993f6427be49acfb3f2577b4caa474
JOHNKYON <zhao_fengbo@outlook.com>
2018-10-23 15:53:56 +0900

    Leetcode354 Accepted.

---
+++ src/questions/leetcode354/RussianDollEnvelopes.java
@@ -0,0 +1,36 @@
+package questions.leetcode354;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class RussianDollEnvelopes {
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes == null ||
+                envelopes.length == 0 ||
+                envelopes[0].length == 0)
+            return 0;
+
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+
+        int n = envelopes.length;
+        int[] dp = new int[n];
+        int res = 0;
+
+        for (int i=0; i<n; i++) {
+            dp[i] = 1;
+            for (int j=0; j<i; j++) {
+                if (envelopes[i][0] > envelopes[j][0] &&
+                        envelopes[i][1] > envelopes[j][1])
+                    dp[i] = Math.max(dp[i], 1+dp[j]);
+            }
+            res = Math.max(res, dp[i]);
+        }
+
+        return res;
+    }
+}
