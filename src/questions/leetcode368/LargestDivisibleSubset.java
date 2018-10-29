6046a0854a3a598be2a8d402f33de453d1449500
JOHNKYON <zhao_fengbo@outlook.com>
2018-10-29 12:51:51 +0900

    Leetcode368 Improved.

--- src/questions/leetcode368/LargestDivisibleSubset.java
+++ src/questions/leetcode368/LargestDivisibleSubset.java
@@ -41,4 +41,37 @@ public class LargestDivisibleSubset {
 
         return res;
     }
+
+    public List<Integer> largestDivisibleSubset2(int[] nums) {
+        List<Integer> r = new ArrayList<>();
+        if (nums.length == 0) return r;
+        Arrays.sort(nums);
+
+        int[] dp = new int[nums.length];
+        int[] parent = new int[nums.length];
+        Arrays.fill(parent, -1);
+        Arrays.fill(dp, 1);
+        int max=0;
+        int maxIndex=0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if(nums[i]%nums[j]==0) {
+                    if (dp[i] < dp[j] + 1) {
+                        dp[i] = dp[j] + 1;
+                        parent[i] = j;
+                    }
+                }
+            }
+            if (dp[i] > max) {
+                max = dp[i];
+                maxIndex = i;
+            }
+        }
+
+        while (maxIndex >= 0) {
+            r.add(nums[maxIndex]);
+            maxIndex = parent[maxIndex];
+        }
+        return r;
+    }
 }
