d54b438eb9f5579ea27975736c2e3c2dbf0d3f59
JOHNKYON <zhao_fengbo@outlook.com>
2018-10-29 11:42:57 +0900

    Leetcode727 Improved.

--- src/questions/leetcode727/MinimumWindowSubsequence.java
+++ src/questions/leetcode727/MinimumWindowSubsequence.java
@@ -1,5 +1,9 @@
 package questions.leetcode727;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MinimumWindowSubsequence {
     public String minWindow(String S, String T) {
         if (S == null || S.length() == 0 ||
@@ -41,5 +45,47 @@ public class MinimumWindowSubsequence {
             }
         }
         return (right < Integer.MAX_VALUE)?S.substring(left, right+1): "";
+    }
+
+    public String minWindow2(String S, String T) {
+        int N = S.length();
+        int[] last = new int[26];
+        int[][] nxt = new int[N][26];
+        Arrays.fill(last, -1);
+
+        for (int i = N - 1; i >= 0; --i) {
+            last[S.charAt(i) - 'a'] = i;
+            for (int k = 0; k < 26; ++k) {
+                nxt[i][k] = last[k];
+            }
+        }
+
+        List<int[]> windows = new ArrayList();
+        for (int i = 0; i < N; ++i) {
+            if (S.charAt(i) == T.charAt(0))
+                windows.add(new int[]{i, i});
+        }
+        for (int j = 1; j < T.length(); ++j) {
+            int letterIndex = T.charAt(j) - 'a';
+            for (int[] window: windows) {
+                if (window[1] < N-1 && nxt[window[1]+1][letterIndex] >= 0) {
+                    window[1] = nxt[window[1]+1][letterIndex];
+                }
+                else {
+                    window[0] = window[1] = -1;
+                    break;
+                }
+            }
+        }
+
+        int[] ans = {-1, S.length()};
+        for (int[] window: windows) {
+            if (window[0] == -1) break;
+            if (window[1] - window[0] < ans[1] - ans[0]) {
+                ans = window;
+            }
+
+        }
+        return ans[0] >= 0 ? S.substring(ans[0], ans[1] + 1) : "";
     }
 }
