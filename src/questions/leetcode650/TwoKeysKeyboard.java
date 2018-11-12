ae40c9db79b8d8796f192f75a38040461523a9f5
JOHNKYON <zhao_fengbo@outlook.com>
2018-11-12 18:42:15 +0900

    Leetcode650 Improved.

--- src/questions/leetcode650/TwoKeysKeyboard.java
+++ src/questions/leetcode650/TwoKeysKeyboard.java
@@ -14,4 +14,16 @@ public class TwoKeysKeyboard {
         }
         return dp[n];
     }
+
+    public int minStepsII(int n) {
+        int res = 0, d = 2;
+        while (n > 1) {
+            while (n % d == 0) {
+                res += d;
+                n /= d;
+            }
+            d++;
+        }
+        return res;
+    }
 }
