2a8f85c28e4344d62514ebf14c684b713088c5bb
JOHNKYON <zhao_fengbo@outlook.com>
2018-12-13 14:09:00 +0900

    Leetcode266 Accepted.

---
+++ src/questions/leetcode266/PalindromePermutation.java
@@ -0,0 +1,17 @@
+package questions.leetcode266;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PalindromePermutation {
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        for (char c: s.toCharArray()) {
+            if (set.contains(c))
+                set.remove(c);
+            else
+                set.add(c);
+        }
+        return set.size() <= 1;
+    }
+}
