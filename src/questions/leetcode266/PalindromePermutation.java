0dc04889d00245bc124fad3c1e614e06e531915f
JOHNKYON <zhao_fengbo@outlook.com>
2018-12-13 14:11:42 +0900

    Leetcode266 Improved.

--- src/questions/leetcode266/PalindromePermutation.java
+++ src/questions/leetcode266/PalindromePermutation.java
@@ -14,4 +14,19 @@ public class PalindromePermutation {
         }
         return set.size() <= 1;
     }
+
+    public boolean canPermutePalindrome2(String s) {
+        char [] map = new char[130];
+        int odd = 0;
+        for(int i = 0; i < s.length(); i++) {
+            map[s.charAt(i)]++;
+            if ((map[s.charAt(i)] & 1) == 0)
+                odd--;
+            else
+                odd++;
+        }
+
+
+        return odd <= 1;
+    }
 }
