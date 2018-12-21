ecf056f571b42d29af896f87ab9c82f5a1d74004
JOHNKYON <zhao_fengbo@outlook.com>
2018-12-21 12:36:27 +0900

    Leetcode69 Accepted.

---
+++ src/questions/leetcode69/Sqrtx.java
@@ -0,0 +1,10 @@
+package questions.leetcode69;
+
+public class Sqrtx {
+    public int mySqrt(int x) {
+        long r = x;
+        while (r*r > x)
+            r = (r + x/r) / 2;
+        return (int) r;
+    }
+}
