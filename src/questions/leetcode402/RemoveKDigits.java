3f4a0d32d55c20d21c52c160bb2bcdf5b9ac905e
JOHNKYON <zhao_fengbo@outlook.com>
2019-01-18 12:22:26 +0900

    Leetcode402 Improved.

--- src/questions/leetcode402/RemoveKDigits.java
+++ src/questions/leetcode402/RemoveKDigits.java
@@ -1,5 +1,7 @@
 package questions.leetcode402;
 
+import java.util.Stack;
+
 public class RemoveKDigits {
     public String removeKdigits(String num, int k) {
         StringBuilder sb = new StringBuilder(num);
@@ -22,5 +24,36 @@ public class RemoveKDigits {
             return "0";
         String ans = sb.substring(i, sb.length());
         return ans;
+    }
+
+    public String removeKdigits2(String num, int k) {
+        if (k == num.length())
+            return "0";
+
+        char[] chs = num.toCharArray();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i=0; i<chs.length; i++) {
+            if (stack.isEmpty()) {
+                stack.push(chs[i]);
+            } else {
+                while (k > 0 && !stack.isEmpty() && stack.peek() > chs[i]) {
+                    stack.pop();
+                    k--;
+                }
+                stack.push(chs[i]);
+            }
+        }
+        while (k-- > 0)
+            stack.pop();
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty())
+            sb.append(stack.pop());
+        sb.reverse();
+        int i=0;
+        while (sb.length() > 1 && sb.charAt(0) == '0')
+            sb.deleteCharAt(0);
+        return sb.toString();
     }
 }
