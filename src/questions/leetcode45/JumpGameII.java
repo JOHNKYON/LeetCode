2be7759509f21e21107ba3959f7591e73c77aaa3
JOHNKYON <zhao_fengbo@outlook.com>
2019-01-31 15:24:17 +0900

    Leetcode45 Improved.

--- src/questions/leetcode45/JumpGameII.java
+++ src/questions/leetcode45/JumpGameII.java
@@ -17,4 +17,18 @@ public class JumpGameII {
         }
         return memo[0] == Integer.MAX_VALUE? -1: memo[0];
     }
+
+    public int jump2(int[] nums) {
+        int end = 0, furthest = 0;
+        int ans = 0;
+        for (int i = 0; i<nums.length-1; i++) {
+            furthest = Math.max(furthest, nums[i] + i);
+            if (i == end) {
+                ans++;
+                end = furthest;
+            }
+        }
+
+        return ans;
+    }
 }
