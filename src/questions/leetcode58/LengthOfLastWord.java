760c200168cb88435afcebc8115d328df2dba01f
JOHNKYON <zhaofengbo1321@163.com>
2019-04-11 17:11:06 +0900

    Leetcode58 Solution2 Accepted.

--- src/questions/leetcode58/LengthOfLastWord.java
+++ src/questions/leetcode58/LengthOfLastWord.java
@@ -7,4 +7,16 @@ public class LengthOfLastWord {
             return 0;
         return words[words.length-1].length();
     }
+
+    public int lengthOfLastWord2(String s) {
+        int right = s.length() - 1;
+        while (right >= 0 && s.charAt(right) == ' ')
+            right--;
+        if (right < 0)
+            return 0;
+        int left = right;
+        while (left >= 0 && s.charAt(left) != ' ')
+            left--;
+        return right - left;
+    }
 }
