1ee4bb9e2eb4def9ccaa53127f18c5f848abc7cb
JOHNKYON <zhaofengbo1321@163.com>
2019-04-12 18:33:15 +0900

    Leetcode171 Accepted.

---
+++ src/questions/leetcode171/ExcelSheetColumnNumber.java
@@ -0,0 +1,12 @@
+package questions.leetcode171;
+
+public class ExcelSheetColumnNumber {
+    public int titleToNumber(String s) {
+        int num = 0;
+        for (int i=0; i<s.length(); i++) {
+            num *= 26;
+            num += s.charAt(i) - 'A' + 1;
+        }
+        return num;
+    }
+}
