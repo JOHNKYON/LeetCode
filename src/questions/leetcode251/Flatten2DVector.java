62d1e252abe60197299519422a89e998995c5599
JOHNKYON <zhaofengbo1321@163.com>
2019-04-15 16:41:48 +0900

    Leetcode251 Accepted.

---
+++ src/questions/leetcode251/Flatten2DVector.java
@@ -0,0 +1,33 @@
+package questions.leetcode251;
+
+public class Flatten2DVector {
+    int i;
+    int j;
+    int[][] v;
+
+    public Vector2D(int[][] v) {
+        this.v = v;
+        i = 0;
+        j = -1;
+    }
+
+    public int next() {
+        if (hasNext()) {
+            j++;
+            while(j == v[i].length) {
+                j = 0;
+                i++;
+            }
+        }
+        return v[i][j];
+    }
+
+    public boolean hasNext() {
+        int ni = i, nj = j+1;
+        while(ni < v.length && nj == v[ni].length) {
+            nj=0;
+            ni++;
+        }
+        return ni != v.length;
+    }
+}
